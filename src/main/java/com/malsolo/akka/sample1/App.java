3807fdee24033e1373175e9ff3a1a2cc66b923b4
Javier Beneito Barquero <jbbarquero@gmail.com>
2017-02-23 18:09:52 +0100

    Akka-java8: first simple sample. With concurrency.

--- src/main/java/com/malsolo/akka/sample1/App.java
+++ src/main/java/com/malsolo/akka/sample1/App.java
@@ -4,16 +4,31 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws InterruptedException {
 
         ActorSystem system = ActorSystem.create("sample1");
 
         final ActorRef counter = system.actorOf(Counter.props(), "counter");
 
-        counter.tell(new Counter.Message(), ActorRef.noSender());
+        //counter.tell(new Counter.Message(), ActorRef.noSender());
 
+        //@formatter:off
+        IntStream
+                .range(0, 5)
+                .forEach(
+                        i -> new Thread(
+                                () -> IntStream
+                                        .range(0, 5)
+                                .forEach(j -> counter.tell(new Counter.Message(), ActorRef.noSender()))
+                        ).start()
+        );
+        //@formatter:on
+
+        TimeUnit.MILLISECONDS.sleep(500);
         System.out.println("ENTER to terminate");
         try (Scanner scanner = new Scanner(System.in)) {
             scanner.nextLine();
